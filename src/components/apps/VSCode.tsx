060b493d9d10abc4fae278b2ff6ffa4248604f2b
walletkun <feilinpersonal@gmail.com>
2025-03-29 17:56:24 -0400

    finished setting up the vscode correctly modified it to readonly instead of actually interacting

--- src/components/apps/VSCode.tsx
+++ src/components/apps/VSCode.tsx
@@ -2,7 +2,7 @@ export default function VSCode() {
   return (
     <iframe
       className="size-full bg-[#202020]"
-      src="https://github.dev/walletkun/react_portfolio/blob/main/README.md"
+      src="https://github1s.com/walletkun/react_portfolio/blob/main/README.md"
       title="VSCode"
     />
   );
